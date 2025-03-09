02ef8d00b992d30904b1a0604a7788976b5a4ff6
othman geurrar <oteman.raja@gmail.com>
2025-03-09 14:11:25 +0000

    add nav vid background and languages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+import withVideos from 'next-videos';
+import createNextIntlPlugin from 'next-intl/plugin';
+
 const nextConfig = {};
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(withVideos(nextConfig));
---
+++ src/app/[locale]/page.js
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/hero-section";
+
+
+export default async function Home() {
+
+
+  return (
+    <main>
+      <HeroSection />
+      
+    </main>
+  );
+}
