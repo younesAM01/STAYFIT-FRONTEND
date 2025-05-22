d7ff5b1da036e5bdcccb254ebbd807c8bb4a0017
younesAM01 <younesaitmanssour@gmail.com>
2025-05-23 00:18:16 +0100

    email confirmation

--- src/app/[locale]/(main-layout)/auth/confirmation-email/page.js
+++ src/app/[locale]/(main-layout)/auth/confirmation-email/page.js
@@ -6,12 +6,12 @@ export default function EmailSentPage() {
   const t = useTranslations('Auth');
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center bg-[#0d111a]">
       <div className="text-center p-8">
-        <h1 className="text-2xl font-bold mb-4">
+        <h1 className="text-2xl font-bold mb-4 text-[#B4E90E]">
           {t('emailSent')}
         </h1>
-        <p className="text-gray-600">
+        <p className="text-white">
           {t('checkEmailInstructions')}
         </p>
       </div>
